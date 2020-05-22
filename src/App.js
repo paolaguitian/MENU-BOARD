6d6e931034f0f81cf85ef72a37148200bef0478b
Paola Guitian <poliguitian@gmail.com>
2020-05-21 22:37:25 -0400

    update needed data into one single structure

--- src/App.js
+++ src/App.js
@@ -1,46 +1,73 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from 'axios';
-
+import Navbar from './components/Navbar/Navbar';
+import MainMenu from './components/MainMenu';
 
 const App = () => {
-  const [category, setCategory] = useState('');
+  const [data, setData] = useState('');
 
   useEffect(() => {
-    const menuReq = axios.get('/api/menu')
-    const sectionReq = axios.get('/api/sections')
-
     const fetchData = async () => {
-      await Promise.all([menuReq, sectionReq]).then(([res1, res2] = []) => {
-        const menu = res1.data.options.map(({ _key } = {}) => _key)
-        const sections = res2.data.reduce((acc, item) => {
-          if (menu.includes(item._id)) {
-            return [
-              ...acc,
-              {
+      const [menuReq, sectionReq, itemReq] = await Promise.all([
+        axios.get('/api/menu'),
+        axios.get('/api/sections'),
+        axios.get('/api/items')
+      ]).catch(console.error)
+
+      const menuIds = menuReq.data.options.map(({ _key } = {}) => _key);
+      const allData = sectionReq.data.reduce((acc, item) => {
+        //grab sections only on the given menu (BK)
+        if (menuIds.includes(item._id)) { //refactor to .some
+          const sectionName = item.name.en;
+          const routeId = sectionName.split(' ').join('-'); //made for router id
+          const sectionItemsId = item.options.map(({ _key }) => _key);
+
+          //grab items from sections
+          const sectionItems = itemReq.data.reduce((acc, item) => {
+            if (sectionItemsId.includes(item._id)) { //refactor to .some
+              acc[item._id] = {
                 name: item.name.en,
-                carouselImage: item.carouselImage,
-                image: item.image,
-                itemKey: item.options.map(({ _key }) => _key),
+                image: item.image.asset._ref,
               }
-            ]
+            }
+            return acc;
+          }, {})
+
+          //Cleand/Needed Data: section name, section image, and items
+          acc[routeId] = {
+            name: sectionName,
+            carouselImage: item.carouselImage,
+            image: item.image,
+            items: sectionItems
           }
-          return acc;
-        }, [])
-        setCategory(sections);
-      })
+        }
+
+        return acc;
+      }, {})
+      setData(allData);
     }
 
     fetchData();
   }, []);
 
-  console.log(category);
+  console.log(data);
   return (
-    <div>
-      hellooo
-      testing
+    <Router>
+      <Switch>
+        <div className="main">
+          <Navbar
+            items={data}
+          />
+          <Route exact path='/' component={MainMenu} />
+          <Route exact path='/menu/:section' component={MainMenu} />
+        </div>
+      </Switch>
 
-    </div>
+    </Router>
   )
 }
+
+
 
 export default App;
