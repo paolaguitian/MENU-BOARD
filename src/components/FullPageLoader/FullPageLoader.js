27be30eff77b7902e04fd5a0463cda6cbc5f9434
Paola Guitian <poliguitian@gmail.com>
2020-05-22 19:24:44 -0400

    full page loader

---
+++ src/components/FullPageLoader/FullPageLoader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FullPageLoader.css';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const FullPageLoader = () => {
+
+  const loadingIcon = <LoadingOutlined style={{ fontSize: 70 }} spin />;
+
+  return (
+    <div className="load-screen">
+      <Spin indicator={loadingIcon} />
+       LET'S EAT
+      <img className="icon" src="/images/logo.png" alt="BK" />
+    </div>
+  )
+}
+export default FullPageLoader;
