a3b8c58f6fc92de6becc51ac9fc89aef2c639793
Paola Guitian <poliguitian@gmail.com>
2020-05-22 14:36:00 -0400

    basic navbar function

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = (props) => {
+
+  //TODO move logic to parent
+  if (props.data === '') {
+    return 'loading';
+  }
+
+  const { data } = props;
+  const values = Object.values(data);
+
+  return (
+    <div className="navbar">
+      {values.map(item => (
+        <NavLink
+          className="tile"
+          key={`${item.name}-link`}
+          to={`/${item.routeId}`}
+          activeStyle={{ color: "#EE1D23" }}
+        >
+          <img className="icon" src={`/images/${item.image}`} alt="" />
+          <div className="section">{item.name}</div>
+        </NavLink>
+      ))
+      }
+    </div >
+  )
+}
+
+export default Navbar;
